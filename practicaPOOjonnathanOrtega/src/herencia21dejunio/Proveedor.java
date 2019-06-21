03eec7acd2f03409d53c36c88fc92a0739dfcf67
ISTLOJA12 <ISTLOJA12@ISTLOJA01>
2019-06-21 15:49:57 +0100

    Herencia21dejunio

--- practicaPOOjonnathanOrtega/src/herencia21dejunio/Proveedor.java
+++ practicaPOOjonnathanOrtega/src/herencia21dejunio/Proveedor.java
@@ -9,6 +9,38 @@ package herencia21dejunio;
  *
  * @author ISTLOJA12
  */
-public class Proveedor {
+public class Proveedor extends Cliente {
+    private String vender;
+    private String produccion;
+    public void vender(){
+        System.out.println("como proveedor te vendo mis productos");
+        
+        
+    }
+
+    public String getVender() {
+        return vender;
+    }
+
+    public void setVender(String vender) {
+        this.vender = vender;
+    }
+
+    public String getProduccion() {
+        return produccion;
+    }
+    public void produccion(){
+        System.out.println("la produccion de la empresa");
+    }
+
+    public void setProduccion(String produccion) {
+        this.produccion = produccion;
+    }
+    public static void main(String[] args) {
+        Proveedor objeto=new Proveedor();
+        objeto.saludar();
+        objeto.vender();
+        objeto.produccion();
+    }
     
 }
